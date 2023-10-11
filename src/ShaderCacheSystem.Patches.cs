6998d9f0a95dac93a13ab380b9d19b5018cf4b0f
Xytabich <7714420+Xytabich@users.noreply.github.com>
2023-10-11 19:30:34 +0300

    init

---
+++ src/ShaderCacheSystem.Patches.cs
@@ -0,0 +1,231 @@
+﻿using HarmonyLib;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.Client;
+using Vintagestory.Client.NoObf;
+using XXHash;
+
+namespace PowerOfMind.ShaderCache
+{
+	public partial class ShaderCacheSystem
+	{
+		private static bool CompileShaderPrefix(ShaderProgram __instance, ref bool __result, out bool __state)
+		{
+			__state = false;
+			if(system != null && __instance.LoadFromFile)
+			{
+				var key = new AssetLocation(__instance.AssetDomain, __instance.PassName);
+				var hashes = new List<ShaderHashInfo>(3);
+				if(!CollectShaderHashes(__instance, hashes)) return true;
+
+				if(system.HasCachedShaderProgram(key) && system.TryLoadCachedShaderProgram(key, hashes, out __instance.ProgramId, out _))
+				{
+					var uniformNames = new HashSet<string>();
+					if(__instance.VertexShader != null)
+					{
+						collectShaderUniformNames(__instance, __instance.VertexShader.Code, uniformNames);
+					}
+					if(__instance.FragmentShader != null)
+					{
+						collectShaderUniformNames(__instance, __instance.FragmentShader.Code, uniformNames);
+					}
+					if(__instance.GeometryShader != null)
+					{
+						collectShaderUniformNames(__instance, __instance.GeometryShader.Code, uniformNames);
+					}
+					string notFoundUniforms = "";
+					foreach(string uniformName in uniformNames)
+					{
+						__instance.uniformLocations[uniformName] = ScreenManager.Platform.GetUniformLocation(__instance, uniformName);
+						if(__instance.uniformLocations[uniformName] == -1)
+						{
+							if(notFoundUniforms.Length > 0)
+							{
+								notFoundUniforms += ", ";
+							}
+							notFoundUniforms += uniformName;
+						}
+					}
+					if(notFoundUniforms.Length > 0 && ScreenManager.Platform.GlDebugMode)
+					{
+						ScreenManager.Platform.Logger.Notification("Shader {0}: Uniform locations for variables {1} not found (or not used).", __instance.PassName, notFoundUniforms);
+					}
+					__state = true;
+					__result = true;
+					ScreenManager.Platform.Logger.Notification("Loaded cached shader program for render pass {0}.", key);
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static void CompileShaderPostfix(ShaderProgram __instance, bool __state)
+		{
+			if(system != null && __instance.LoadFromFile && !__state)
+			{
+				var hashes = new List<ShaderHashInfo>(3);
+				if(!CollectShaderHashes(__instance, hashes)) return;
+
+				var key = new AssetLocation(__instance.AssetDomain, __instance.PassName);
+				system.SaveShaderProgramToCache(key, __instance.ProgramId, hashes);
+
+				ScreenManager.Platform.Logger.Notification("Cached shader program for render pass {0}.", key);
+			}
+		}
+
+		private static IEnumerable<CodeInstruction> DisposeShaderTranspiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var insts = new List<CodeInstruction>(instructions);
+			(FieldInfo field, string name)[] fields = {
+				(AccessTools.Field(typeof(ShaderProgramBase), nameof(ShaderProgramBase.VertexShader)), nameof(ShaderProgramBase.VertexShader)),
+				(AccessTools.Field(typeof(ShaderProgramBase), nameof(ShaderProgramBase.FragmentShader)), nameof(ShaderProgramBase.FragmentShader)),
+				(AccessTools.Field(typeof(ShaderProgramBase), nameof(ShaderProgramBase.GeometryShader)), nameof(ShaderProgramBase.GeometryShader))
+			};
+			for(int i = 0; i < insts.Count; i++)
+			{
+				if(i + 2 < insts.Count)
+				{
+					if(insts[i].opcode == OpCodes.Ldarg_0 && (insts[i + 2].opcode == OpCodes.Brfalse || insts[i + 2].opcode == OpCodes.Brfalse_S))
+					{
+						foreach(var info in fields)
+						{
+							if(insts[i + 1].LoadsField(info.field))
+							{
+								insts[i + 2] = new CodeInstruction(OpCodes.Brfalse, insts[i + 2].operand);
+								insts.InsertRange(i + 3, new CodeInstruction[] {
+									CodeInstruction.LoadArgument(0),
+									CodeInstruction.LoadField(typeof(ShaderProgramBase), info.name),
+									CodeInstruction.LoadField(typeof(Shader), nameof(Shader.ShaderId)),
+									new CodeInstruction(OpCodes.Brfalse, insts[i + 2].operand)
+								});
+								i += 7;
+								break;
+							}
+						}
+					}
+				}
+			}
+			return insts;
+		}
+
+		private static bool CollectShaderHashes(ShaderProgramBase shader, ICollection<ShaderHashInfo> hashes)
+		{
+			ShaderHashInfo hash;
+			if(shader.VertexShader != null)
+			{
+				if(!TryGetShaderHash(shader.VertexShader, out hash))
+				{
+					return false;
+				}
+				hashes.Add(hash);
+			}
+			if(shader.FragmentShader != null)
+			{
+				if(!TryGetShaderHash(shader.FragmentShader, out hash))
+				{
+					return false;
+				}
+				hashes.Add(hash);
+			}
+			if(shader.GeometryShader != null)
+			{
+				if(!TryGetShaderHash(shader.GeometryShader, out hash))
+				{
+					return false;
+				}
+				hashes.Add(hash);
+			}
+			return true;
+		}
+
+		private static bool TryGetShaderHash(Shader shader, out ShaderHashInfo info)
+		{
+			string shaderCode = shader.Code;
+			if(!string.IsNullOrEmpty(shaderCode))
+			{
+				int approxSize = shaderCode.Length + 16;
+				if(!string.IsNullOrEmpty(shader.PrefixCode)) approxSize += shader.PrefixCode.Length;
+				int versionIndex = Math.Max(0, shaderCode.IndexOf("#version"));
+				int newLineIndex = -1;
+
+				var sb = new StringBuilder(approxSize);
+				if(RuntimeEnv.OS == OS.Mac)
+				{
+					sb.Append("#version 330");
+					if(versionIndex < 0)
+					{
+						sb.AppendLine();
+					}
+				}
+				else if(versionIndex >= 0)
+				{
+					newLineIndex = shaderCode.IndexOf("\n", versionIndex);
+					sb.Append(shaderCode, 0, newLineIndex + 1);
+				}
+				sb.Append(shader.PrefixCode);
+				sb.Append(shaderCode, (newLineIndex + 1), shaderCode.Length - (newLineIndex + 1));
+
+				ulong hash = 0;
+				if(sb.Length > approxSize)
+				{
+					hash = XXHash64.Hash(sb.ToString());
+				}
+				else
+				{
+					foreach(var chunk in sb.GetChunks())
+					{
+						hash = XXHash64.Hash(chunk.Span);
+						break;
+					}
+				}
+
+				info = new ShaderHashInfo(shader.Type, hash, sb.Length);
+				return true;
+			}
+
+			info = default;
+			return false;
+		}
+
+		private static IEnumerable<CodeInstruction> CreateShaderTranspiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var insts = new List<CodeInstruction>(instructions);
+			var linkMethod = typeof(GL).GetMethod("LinkProgram", BindingFlags.Static | BindingFlags.Public, new Type[] { typeof(int) });
+			for(int i = 0; i < insts.Count; i++)
+			{
+				if(i + 1 < insts.Count)
+				{
+					if(insts[i].IsLdloc() && insts[i + 1].Calls(linkMethod))
+					{
+						var ldarg = CodeInstruction.LoadArgument(0);
+						ldarg.labels.AddRange(insts[i].labels);
+
+						insts[i].labels.Clear();
+
+						insts.InsertRange(i, new CodeInstruction[] {
+							ldarg,
+							new CodeInstruction(insts[i]),
+							CodeInstruction.Call(() => SetShaderBinProgramFlag)
+						});
+						break;
+					}
+				}
+			}
+			return insts;
+		}
+
+		private static void SetShaderBinProgramFlag(ShaderProgram shader, int handle)
+		{
+			if(system != null && shader.LoadFromFile && system.HasProgramBinaryArb())
+			{
+				GL.ProgramParameter(handle, ProgramParameterName.ProgramBinaryRetrievableHint, (int)All.True);
+			}
+		}
+	}
+}
---
+++ src/ShaderCacheSystem.cs
@@ -0,0 +1,333 @@
+﻿using HarmonyLib;
+using Newtonsoft.Json;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.Client.NoObf;
+
+namespace PowerOfMind.ShaderCache
+{
+	public partial class ShaderCacheSystem : ModSystem
+	{
+		private const byte SHADER_CACHE_VERSION = 1;
+		private static readonly Action<ShaderProgram, string, HashSet<string>> collectShaderUniformNames = null;
+
+		private static ShaderCacheSystem system = null;
+
+		static ShaderCacheSystem()
+		{
+			var shader = Expression.Parameter(typeof(ShaderProgram));
+			var code = Expression.Parameter(typeof(string));
+			var list = Expression.Parameter(typeof(HashSet<string>));
+			collectShaderUniformNames = Expression.Lambda<Action<ShaderProgram, string, HashSet<string>>>(
+				Expression.Call(shader, "collectUniformNames", null, code, list),
+				shader, code, list
+			).Compile();
+		}
+
+		private Dictionary<AssetLocation, int> cachedShaders = null;
+		private Stack<int> freeEntries = null;
+		private int entriesCounter = 0;
+
+		private bool shaderCacheDirty = false;
+		private HashSet<ShaderHashInfo> tmpShaderHashSet = null;
+
+		private bool? arbGetProgramBinary = null;
+
+		private Harmony harmony = null;
+		private ICoreClientAPI api = null;
+
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			system = this;
+			api.Event.ReloadShader += OnReloadShaders;
+			harmony = new Harmony("pomshadercache");
+
+			harmony.Patch(typeof(ShaderProgram).GetMethod(nameof(ShaderProgram.Compile), BindingFlags.Instance | BindingFlags.Public),
+				prefix: new HarmonyMethod(SymbolExtensions.GetMethodInfo(() => CompileShaderPrefix)),
+				postfix: new HarmonyMethod(SymbolExtensions.GetMethodInfo(() => CompileShaderPostfix)));
+			harmony.Patch(typeof(ShaderProgramBase).GetMethod(nameof(ShaderProgramBase.Dispose), BindingFlags.Instance | BindingFlags.Public),
+				transpiler: new HarmonyMethod(SymbolExtensions.GetMethodInfo(() => DisposeShaderTranspiler)));
+			harmony.Patch(typeof(ClientPlatformWindows).GetMethod(nameof(ClientPlatformWindows.CreateShaderProgram), BindingFlags.Instance | BindingFlags.Public),
+				transpiler: new HarmonyMethod(SymbolExtensions.GetMethodInfo(() => CreateShaderTranspiler)));
+		}
+
+		public override void Dispose()
+		{
+			if(api != null)
+			{
+				if(system == this) system = null;
+				harmony?.UnpatchAll("pomshadercache");
+				harmony = null;
+				SaveShadersCache();
+			}
+		}
+
+		public bool HasCachedShaderProgram(AssetLocation shaderKey)
+		{
+			if(cachedShaders == null) LoadShadersCache();
+			return cachedShaders.ContainsKey(shaderKey);
+		}
+
+		public unsafe bool HasProgramBinaryArb()
+		{
+			if(!arbGetProgramBinary.HasValue)
+			{
+				int num = GL.GetInteger(GetPName.NumProgramBinaryFormats);
+				arbGetProgramBinary = GL.GetError() == ErrorCode.NoError && num > 0;
+			}
+			return arbGetProgramBinary.Value;
+		}
+
+		public void SaveShadersCache()
+		{
+			if(cachedShaders != null && shaderCacheDirty)
+			{
+				shaderCacheDirty = false;
+				try
+				{
+					var path = Path.Combine(GamePaths.Cache, "pomshadercache");
+					if(!Directory.Exists(path))
+					{
+						Directory.CreateDirectory(path);
+					}
+					path = Path.Combine(path, "entries.json");
+					File.WriteAllText(path, JsonConvert.SerializeObject(new CacheEntries() {
+						cache = cachedShaders,
+						free = freeEntries,
+						counter = entriesCounter
+					}));
+				}
+				catch { }
+			}
+		}
+
+		public unsafe bool TryLoadCachedShaderProgram<TEnumerable>(AssetLocation shaderKey, TEnumerable stagesHash, out int handle, out string error)
+			where TEnumerable : IEnumerable<ShaderHashInfo>
+		{
+			if(!HasProgramBinaryArb())
+			{
+				handle = 0;
+				error = "Not supported";
+				return false;
+			}
+
+			if(cachedShaders == null) LoadShadersCache();
+
+			handle = 0;
+			error = null;
+			if(cachedShaders.TryGetValue(shaderKey, out var id))
+			{
+				string path = null;
+				try
+				{
+					path = Path.Combine(GamePaths.Cache, "pomshadercache", (id & 255).ToString("x2"), id.ToString("x8"));
+
+					if(File.Exists(path))
+					{
+						using(var stream = File.OpenRead(path))
+						{
+							if(stream.ReadByte() == SHADER_CACHE_VERSION)
+							{
+								if(tmpShaderHashSet == null) tmpShaderHashSet = new HashSet<ShaderHashInfo>();
+								else tmpShaderHashSet.Clear();
+								tmpShaderHashSet.UnionWith(stagesHash);
+
+								int stageCount = stream.ReadByte();
+								if(stageCount == tmpShaderHashSet.Count)
+								{
+									ulong tmpLong;
+									var buffSpan = new Span<byte>(&tmpLong, sizeof(ulong));
+									for(int i = 0; i < stageCount; i++)
+									{
+										stream.Read(buffSpan);
+										int size = ((int*)&tmpLong)[0];
+										var type = (EnumShaderType)((int*)&tmpLong)[1];
+
+										stream.Read(buffSpan);
+										tmpShaderHashSet.Add(new ShaderHashInfo(type, tmpLong, size));
+									}
+
+									// Since the size are the same, no new elements were added, so the shader probably hasn’t changed
+									if(stageCount == tmpShaderHashSet.Count)
+									{
+										stream.Read(buffSpan);
+										int size = ((int*)&tmpLong)[0];
+										int format = ((int*)&tmpLong)[1];
+
+										handle = GL.CreateProgram();
+
+										var bytes = ArrayPool<byte>.Shared.Rent(size);
+										stream.Read(bytes, 0, size);
+										GL.ProgramBinary(handle, (BinaryFormat)format, bytes, size);
+										ArrayPool<byte>.Shared.Return(bytes);
+
+										var errno = GL.GetError();
+										if(errno == ErrorCode.NoError)
+										{
+											GL.GetProgram(handle, GetProgramParameterName.LinkStatus, out var status);
+											if(status == (int)All.True)
+											{
+												return true;
+											}
+										}
+
+										error = errno.ToString();
+									}
+								}
+							}
+						}
+					}
+				}
+				catch(Exception e)
+				{
+					error = e.Message;
+				}
+
+				if(handle != 0)
+				{
+					GL.DeleteProgram(handle);
+					handle = 0;
+				}
+
+				try
+				{
+					if(!string.IsNullOrEmpty(path) && File.Exists(path))
+					{
+						File.Delete(path);
+					}
+				}
+				catch { }
+
+				freeEntries.Push(id);
+				cachedShaders.Remove(shaderKey);
+			}
+
+			return false;
+		}
+
+		public unsafe void SaveShaderProgramToCache<TEnumerable>(AssetLocation shaderKey, int handle, TEnumerable stagesHash)
+			where TEnumerable : IEnumerable<ShaderHashInfo>
+		{
+			if(!HasProgramBinaryArb()) return;
+
+			if(tmpShaderHashSet == null) tmpShaderHashSet = new HashSet<ShaderHashInfo>();
+			else tmpShaderHashSet.Clear();
+			tmpShaderHashSet.UnionWith(stagesHash);
+			if(tmpShaderHashSet.Count == 0) return;
+
+			GL.GetProgram(handle, GetProgramParameterName.ProgramBinaryRetrievableHint, out var state);
+			if(GL.GetError() != ErrorCode.NoError || state != (int)All.True) return;
+			GL.GetProgram(handle, GetProgramParameterName.ProgramBinaryLength, out var size);
+			if(GL.GetError() != ErrorCode.NoError || size == 0) return;
+
+			if(cachedShaders == null) LoadShadersCache();
+			if(!cachedShaders.TryGetValue(shaderKey, out var id))
+			{
+				if(freeEntries.Count > 0)
+				{
+					id = freeEntries.Pop();
+				}
+				else
+				{
+					id = entriesCounter++;
+				}
+				cachedShaders[shaderKey] = id;
+			}
+
+			try
+			{
+				var bytes = ArrayPool<byte>.Shared.Rent(size);
+				GL.GetProgramBinary(handle, size, out size, out BinaryFormat format, bytes);
+				if(GL.GetError() != ErrorCode.NoError)
+				{
+					ArrayPool<byte>.Shared.Return(bytes);
+					return;
+				}
+
+				var path = Path.Combine(GamePaths.Cache, "pomshadercache", (id & 255).ToString("x2"));
+				if(!Directory.Exists(path)) Directory.CreateDirectory(path);
+				path = Path.Combine(path, id.ToString("x8"));
+				using(var stream = File.OpenWrite(path))
+				{
+					stream.SetLength(0);
+					stream.WriteByte(SHADER_CACHE_VERSION);
+
+					ulong tmpLong;
+					var buffSpan = new Span<byte>(&tmpLong, sizeof(ulong));
+
+					stream.WriteByte((byte)tmpShaderHashSet.Count);
+					foreach(var info in tmpShaderHashSet)
+					{
+						((int*)&tmpLong)[0] = info.Size;
+						((int*)&tmpLong)[1] = (int)info.Type;
+						stream.Write(buffSpan);
+
+						tmpLong = info.Hash;
+						stream.Write(buffSpan);
+					}
+
+					((int*)&tmpLong)[0] = size;
+					((int*)&tmpLong)[1] = (int)format;
+					stream.Write(buffSpan);
+
+					stream.Write(bytes, 0, size);
+
+					shaderCacheDirty = true;
+				}
+				ArrayPool<byte>.Shared.Return(bytes);
+			}
+			catch { }
+		}
+
+		private void LoadShadersCache()
+		{
+			var path = Path.Combine(GamePaths.Cache, "pomshadercache", "entries.json");
+			entriesCounter = 0;
+			if(File.Exists(path))
+			{
+				try
+				{
+					var entries = JsonConvert.DeserializeObject<CacheEntries>(File.ReadAllText(path));
+					if(entries != null)
+					{
+						cachedShaders = entries.cache;
+						freeEntries = entries.free;
+						entriesCounter = entries.counter;
+					}
+				}
+				catch { }
+			}
+			if(cachedShaders == null)
+			{
+				cachedShaders = new Dictionary<AssetLocation, int>();
+				freeEntries = new Stack<int>();
+				entriesCounter = 0;
+			}
+			else if(freeEntries == null)
+			{
+				freeEntries = new Stack<int>();
+			}
+		}
+
+		private bool OnReloadShaders()
+		{
+			SaveShadersCache();
+			return true;
+		}
+
+		private class CacheEntries
+		{
+			public Dictionary<AssetLocation, int> cache;
+			public Stack<int> free;
+			public int counter;
+		}
+	}
+}
